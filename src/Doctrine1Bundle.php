611d2710c70fead85f79e3afda783ff5cc24bdbf
Jay Klehr <jay@diablomedia.com>
2024-04-30 11:58:13 -0600

    fixing build

--- src/Doctrine1Bundle.php
+++ src/Doctrine1Bundle.php
@@ -8,7 +8,6 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class Doctrine1Bundle extends Bundle
 {
-    /** @return void */
     public function build(ContainerBuilder $container): void
     {
         parent::build($container);
