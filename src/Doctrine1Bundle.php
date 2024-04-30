5b7456db62c89b7f45869add8ce4aeed422d576d
Jay Klehr <jay@diablomedia.com>
2024-04-30 12:03:41 -0600

    Making sure the profiler controller gets removed in builds without the profiler service (#155)
    * Making sure the profiler controller gets removed in builds without the profiler service
    
    * fixing build

--- src/Doctrine1Bundle.php
+++ src/Doctrine1Bundle.php
@@ -2,8 +2,16 @@
 
 namespace DiabloMedia\Bundle\Doctrine1Bundle;
 
+use DiabloMedia\Bundle\Doctrine1Bundle\DependencyInjection\Compiler\RemoveProfilerControllerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class Doctrine1Bundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new RemoveProfilerControllerPass());
+    }
 }
