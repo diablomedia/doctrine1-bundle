37d90823c638cf5db4bd80fad1b096b4b9bce433
Jay Klehr <jay@diablomedia.com>
2024-05-20 15:24:42 -0600

    Adding reset method to Registry (#158)
    * Adding reset method to Registry
    
    * fixing reset method
    
    * fixing phpstan errors

--- src/Registry.php
+++ src/Registry.php
@@ -3,6 +3,7 @@
 namespace DiabloMedia\Bundle\Doctrine1Bundle;
 
 use Doctrine_Connection;
+use Doctrine_Manager;
 use Psr\Container\ContainerInterface;
 
 class Registry
@@ -32,5 +33,18 @@ class Registry
     public function getConnection(string $name = null): Doctrine_Connection
     {
         return $this->container->get('doctrine1.' . $name . '_connection');
+    }
+
+    public function reset(): void
+    {
+        $manager = Doctrine_Manager::getInstance();
+        foreach ($this->connections as $connectionName) {
+            // Connection names come in as "doctrine1.name_connection", we want "name"
+            $connectionName = preg_replace('|_connection$|', '', substr($connectionName, 10));
+            if ($connectionName !== null) {
+                $connection = $manager->getConnection($connectionName);
+                $connection->clear();
+            }
+        }
     }
 }
